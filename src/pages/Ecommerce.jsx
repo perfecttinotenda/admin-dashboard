b1ef38efd868f8b80843715da423ffd21eed346b
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2022-06-12 13:47:37 +0200

    w

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -34,7 +34,7 @@ const Ecommerce = () => {
                 <span className="texl-lg font-semibold">
                   {item.amount}
                 </span>
-                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                <span className={`text-sm text-red-600 ml-2`}>
                   {item.percentage}
                 </span>
               </p>
